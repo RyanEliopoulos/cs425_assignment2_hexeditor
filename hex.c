84ebed2054dad88815daee9fbf21475e5de68715
Ryan Paulos <ryan.eliopoulos@gmail.com>
2020-09-16 05:11:03 -0700

    +arg1 hex parsing
    Memory location for editing is now accepted as a hex string.

--- hex.c
+++ hex.c
@@ -19,16 +19,26 @@ void main(int argc, char* argv[]) {
         exit(2);
     }
 
-    // Move to the designated file offset
-    // Currently requires decimal byte offset.
+    /********* Moving to the designated file offset ********/
+    // Translate hex to unsigned long int
+    unsigned long memaddr;
+    sscanf(argv[1], "%lx", &memaddr);
+    // and adjust file to our offset 
     errno = 0;
-    
-    int ret = fseek(file, atol(argv[1]), SEEK_SET);
+    int ret = fseek(file, memaddr, SEEK_SET);
     if (ret == -1) {
         printf("fseek error: %s\n", strerror(errno));
         exit(3);
     } 
 
+    /* Manual writing for debugging */
+//    int jg = 127;
+//    ret = fwrite(&jg, 1, 1, file);
+//    if (ret != 1) {
+//        printf("fwrite problem\n");
+//        exit(4);
+//    }
+    
     // turn argv[2] from an ASCII representation of hex values
     // to int values we can write to a file
     int len = strlen(argv[2]); 
@@ -46,13 +56,13 @@ void main(int argc, char* argv[]) {
 
 
     // Now writing the new binary values to the file 
-    for (int i=0;i<len;i++) {
-        ret = fwrite(&newvals[i], 1, 1, file);
-        if (ferror(file) != 0) {
-            printf("encountered error writing binary to file\n No guarantee of file state.\n");
-            exit(4);
-        }
-    }
+    //for (int i=0;i<len;i++) {
+        //ret = fwrite(&newvals[i], 1, 1, file);
+        //if (ferror(file) != 0) {
+         //   printf("encountered error writing binary to file\n No guarantee of file state.\n");
+          //  exit(4);
+       // }
+    //}
 
     fclose(file);
 
