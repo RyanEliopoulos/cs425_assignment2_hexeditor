2271cce36770331ea341eafdd324099fb991a412
Ryan Paulos <ryan.eliopoulos@gmail.com>
2020-09-16 05:31:46 -0700

    refactored main into fnxs

--- hex.c
+++ hex.c
@@ -4,7 +4,25 @@
 #include<stdlib.h>
 
 
+
+FILE* writePrep(int, char*, char*);
+void writeBytes(FILE*, char *);
+
 void main(int argc, char* argv[]) {
+    
+    // arg check, open file, fseek offset, 
+    FILE *binaryfile = writePrep(argc, argv[1], argv[3]); 
+
+    writeBytes(binaryfile, argv[3]);
+
+    fclose(binaryfile);
+
+}
+
+/* 
+ *  Returns file pointer loaded at the 
+ *  user-defined offset, ready for writing.  */
+FILE* writePrep(int argc, char *hex_address, char *filepath) {
 
     // Check arg count
     if (argc != 4) {
@@ -13,8 +31,8 @@ void main(int argc, char* argv[]) {
     }
 
     // Open designated binary file
-    FILE *file = fopen(argv[3], "r+b");
-    if (file == NULL) {
+    FILE *filestream = fopen(filepath, "r+b");
+    if (filestream == NULL) {
         printf("error opening file: %s\n", strerror(errno));
         exit(2);
     }
@@ -22,24 +40,21 @@ void main(int argc, char* argv[]) {
     /********* Moving to the designated file offset ********/
     // Translate hex to unsigned long int
     unsigned long memaddr;
-    sscanf(argv[1], "%lx", &memaddr);
+    sscanf(hex_address, "%lx", &memaddr);
     // and adjust file to our offset 
     errno = 0;
-    int ret = fseek(file, memaddr, SEEK_SET);
+    int ret = fseek(filestream, memaddr, SEEK_SET);
     if (ret == -1) {
         printf("fseek error: %s\n", strerror(errno));
         exit(3);
     } 
 
-    /* Manual writing for debugging */
-//    int jg = 127;
-//    ret = fwrite(&jg, 1, 1, file);
-//    if (ret != 1) {
-//        printf("fwrite problem\n");
-//        exit(4);
-//    }
-    
-    // turn argv[2] from an ASCII representation of hex values
+    return filestream; 
+}
+
+void writeBytes(FILE *binaryfile, char *machine_code) {
+
+        // turn argv[2] from an ASCII representation of hex values
     // to int values we can write to a file
     int len = strlen(argv[2]); 
     unsigned int newvals[len]; 
@@ -49,7 +64,7 @@ void main(int argc, char* argv[]) {
     for (int i=0;i<len;i++) {
         tmp = argv[2][i];
         printf("character to translate %c\n", argv[2][i]);
-        ret = sscanf(&tmp, "%x", newvals+i);
+        int ret = sscanf(&tmp, "%x", newvals+i);
         printf("value in newvals: %u\nRet val: %d\n", newvals[i], ret);
     }
 
@@ -63,7 +78,5 @@ void main(int argc, char* argv[]) {
           //  exit(4);
        // }
     //}
-
-    fclose(file);
 
 }
